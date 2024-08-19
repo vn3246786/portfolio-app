06bac532e9217228894e7c6178d3ebc2c8009de5
viraj naik <vn3246786@gmail.com>
2024-08-19 12:42:57 +0530

    update

--- src/Components/AboutMe/AbouteMe.jsx
+++ src/Components/AboutMe/AbouteMe.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./aboutMe.css"
+import { Email, PhoneInTalk } from '@mui/icons-material'
 
-const AbouteMe = ({darkTheme}) => {
+const AbouteMe = () => {
 
 
   return (
     <div className='aboutMe' >
+      <div className="contacts-info">
+        <h1>Contacts</h1>
+        <div className="info">
+        <PhoneInTalk/>
+       <a className='link' href='tel:9588695201'>9588695201</a>
+        </div>
+        <div className="info">
+        <Email/>
+       <a className='link' href='mailto:vn3246786@gmail.com'>vn3246786@gmail.com</a>
+        </div>
+      </div>
 <div className="info-container">
     <div className="title">VIRAJ NAIK</div>
     <div className="subject">I AM A WEB DEVELOPER</div>
